bc970a655a75f5c64675c33a3790bfea4fa7ead0
UntilDone <dbehgus0906@naver.com>
2025-04-03 11:19:44 +0900

    First Step End

---
+++ WpfApp1/ch13.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// ch13.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ch13 : Window
+    {
+        public ch13()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtPath.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(txtPath.Text))
+            {
+                using(StreamReader sr=new StreamReader(txtPath.Text))
+                {
+                    richTxt.Document.Blocks.Clear();
+                    richTxt.AppendText(sr.ReadToEnd());
+                }
+            }
+            else
+            {
+                MessageBox.Show("파일이 없습니다.", "에러", MessageBoxButton.OK);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            richTxt.Document.Blocks.Clear();
+        }
+    }
+}
---
+++ WpfApp1/ch14.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// ch14.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ch14 : Window
+    {
+        public ch14()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File(*.txt)|*.txt|XML File(*.xml)|*.xm|JSON File(*.json)|*.json"; // 저장파일 창에서 필터 설정 Type File(*.Type)|*.Type 형태로 추가하면 된다.
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using(StreamWriter sw=new StreamWriter(saveFileDialog.FileName))
+                {
+                    TextRange textRange = new TextRange(richTxt.Document.ContentStart, richTxt.Document.ContentEnd);
+                    sw.WriteLine(textRange.Text);
+                }
+            }
+            MessageBox.Show("파일이 저장되었습니다.");
+        }
+    }
+}
