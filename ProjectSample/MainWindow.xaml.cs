a2ae4d622b65e16bbd68ed5e281ac63d5842d719
UntilDone <dbehgus0906@naver.com>
2025-04-05 21:30:22 +0900

    new project

---
+++ ProjectSample/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("클릭!", "클릭", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("생성!!!!");
+        }
+    }
+}
