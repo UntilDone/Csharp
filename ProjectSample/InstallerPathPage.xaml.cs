a50b9d61bdcc2529a74b0077a90609ea118d7c68
UntilDone <dbehgus0906@naver.com>
2025-04-07 23:58:44 +0900

    still finding problems

---
+++ ProjectSample/InstallerPathPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Forms;
+namespace InstallerWPF
+{
+    public partial class InstallPathPage : UserControl
+    {
+        public string InstallPath { get; private set; }
+
+        public InstallPathPage()
+        {
+            InitializeComponent();
+
+            // 기본 경로 설정
+            InstallPath = System.IO.Path.Combine(
+                System.Environment.GetFolderPath(System.Environment.SpecialFolder.ProgramFiles),
+                "MyCustomApp");
+
+            PathTextBox.Text = InstallPath;
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "설치할 폴더를 선택하세요";
+                dialog.SelectedPath = InstallPath;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    InstallPath = dialog.SelectedPath;
+                    PathTextBox.Text = InstallPath;
+                }
+            }
+        }
+    }
+}
+}
--- ProjectSample/MainWindow.xaml.cs
+++ ProjectSample/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Win32;
+﻿using InstallerWPF;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Controls;
 using System.Xml;
 
 namespace ProjectSample
@@ -13,117 +15,41 @@ namespace ProjectSample
     /// </summary>
     public partial class MainWindow : Window
     {
-        InstallerConfig _config;
+        private int currentPageIndex = 0;
+        private UserControl[] pages;
 
         public MainWindow()
         {
             InitializeComponent();
-            LoadConfig();
-            InstallPathTextBox.Text = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
-                _config.InstallFolderName
-            );
+
+            // 페이지 초기화
+            pages = new UserControl[]
+            {
+                 new InstallPathPage(),
+                new WelcomePage()
+
+                // 다음 단계에서 추가할 페이지들
+            };
+
+            PageHost.Content = pages[currentPageIndex];
         }
 
-        private void LoadConfig()
+        private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            var config = new InstallerConfig();
-            XmlDocument doc = new XmlDocument();
-            doc.Load("installer-config.xml");
-
-            config.AppName = doc.SelectSingleNode("//AppName")?.InnerText;
-            config.InstallFolderName = doc.SelectSingleNode("//InstallFolderName")?.InnerText;
-
-            config.FilesToInstall = new List<string>();
-            var fileNodes = doc.SelectNodes("//FilesToInstall/File");
-            foreach (XmlNode node in fileNodes)
+            if (currentPageIndex < pages.Length - 1)
             {
-                config.FilesToInstall.Add(node.InnerText);
-            }
-
-            config.CreateDesktopShortcut = bool.Parse(doc.SelectSingleNode("//CreateDesktopShortcut")?.InnerText ?? "false");
-            config.CreateStartMenuShortcut = bool.Parse(doc.SelectSingleNode("//CreateStartMenuShortcut")?.InnerText ?? "false");
-
-            _config = config;
-        }
-
-        private void BrowseButton_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new System.Windows.Forms.FolderBrowserDialog();
-            var result = dialog.ShowDialog();
-            if (result == System.Windows.Forms.DialogResult.OK)
-            {
-                InstallPathTextBox.Text = dialog.SelectedPath;
+                currentPageIndex++;
+                PageHost.Content = pages[currentPageIndex];
             }
         }
 
-        private void InstallButton_Click(object sender, RoutedEventArgs e)
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
         {
-            string targetDir = InstallPathTextBox.Text;
-            Directory.CreateDirectory(targetDir);
-
-            ProgressBar.Value = 0;
-            int fileCount = _config.FilesToInstall.Count;
-            int currentFile = 0;
-
-            foreach (var file in _config.FilesToInstall)
+            if (currentPageIndex > 0)
             {
-                string sourcePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), file);
-                string destPath = Path.Combine(targetDir, Path.GetFileName(file));
-                File.Copy(sourcePath, destPath, overwrite: true);
-
-                currentFile++;
-                ProgressBar.Value = (double)currentFile / fileCount * 100;
-            }
-
-            if (_config.CreateDesktopShortcut)
-                CreateShortcut(targetDir, Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-
-            if (_config.CreateStartMenuShortcut)
-                CreateShortcut(targetDir, Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.StartMenu),
-                    "Programs"
-                ));
-
-            RegisterUninstaller(targetDir);
-
-            MessageBox.Show("설치가 완료되었습니다!", "설치 완료", MessageBoxButton.OK, MessageBoxImage.Information);
-            Close();
-        }
-
-        private void CreateShortcut(string targetDir, string shortcutLocation)
-        {
-            string targetExe = Path.Combine(targetDir, _config.FilesToInstall[0]);
-            string shortcutPath = Path.Combine(shortcutLocation, $"{_config.AppName}.url");
-
-            using (StreamWriter writer = new StreamWriter(shortcutPath))
-            {
-                writer.WriteLine("[InternetShortcut]");
-                writer.WriteLine($"URL=file:///{targetExe.Replace('\\', '/')}");
-                writer.WriteLine($"IconFile={targetExe.Replace('\\', '/')}");
-                writer.WriteLine("IconIndex=0");
+                currentPageIndex--;
+                PageHost.Content = pages[currentPageIndex];
             }
         }
-
-        private void RegisterUninstaller(string installDir)
-        {
-            string uninstallKeyPath = $@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{_config.AppName}";
-            using (RegistryKey uninstallKey = Registry.LocalMachine.CreateSubKey(uninstallKeyPath))
-            {
-                uninstallKey.SetValue("DisplayName", _config.AppName);
-                uninstallKey.SetValue("InstallLocation", installDir);
-                uninstallKey.SetValue("Publisher", "Your Company");
-                uninstallKey.SetValue("UninstallString", Path.Combine(installDir, "Uninstall.exe"));
-            }
-        }
-    }
-
-    public class InstallerConfig
-    {
-        public string AppName { get; set; }
-        public string InstallFolderName { get; set; }
-        public List<string> FilesToInstall { get; set; }
-        public bool CreateDesktopShortcut { get; set; }
-        public bool CreateStartMenuShortcut { get; set; }
     }
 }
