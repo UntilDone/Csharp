fa39d80c09bb265920f3a124addbc61d26dc4cc1
UntilDone <dbehgus0906@naver.com>
2025-04-19 22:30:01 +0900

    rounded

--- HairSalon/WPFProject/MainWindow.xaml.cs
+++ HairSalon/WPFProject/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +20,6 @@ namespace WPFProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
     }
 }
