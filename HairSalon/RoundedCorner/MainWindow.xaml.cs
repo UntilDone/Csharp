6e9b0f68a2ed9798c4a0ade15e47df16538631d9
UntilDone <dbehgus0906@naver.com>
2025-04-20 23:22:49 +0900

    Styling

--- HairSalon/RoundedCorner/MainWindow.xaml.cs
+++ HairSalon/RoundedCorner/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,18 +9,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFProject
+namespace RoundedCorner
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow() => InitializeComponent();
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public MainWindow()
         {
-            MessageBox.Show("로그인을 시도합니다.", "확인");
+            InitializeComponent();
         }
     }
 }
