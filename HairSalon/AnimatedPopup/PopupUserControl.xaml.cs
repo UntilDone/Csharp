aca276065df354d6ee5929ec2cc73f6a6867e0c5
UntilDone <dbehgus0906@naver.com>
2025-04-22 11:48:55 +0900

    popup

--- HairSalon/AnimatedPopup/PopupUserControl.xaml.cs
+++ HairSalon/AnimatedPopup/PopupUserControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChangeButtonImage
+namespace AnimatedPopup
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PopupUserControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PopupUserControl : UserControl
     {
-        public MainWindow()
+        public PopupUserControl()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
         }
     }
 }
