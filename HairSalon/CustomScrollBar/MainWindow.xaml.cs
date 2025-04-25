302a5b68723bd7954869875fc0d7a836a1bcea1a
UntilDone <dbehgus0906@naver.com>
2025-04-25 23:58:44 +0900

    Finish

--- HairSalon/CustomScrollBar/MainWindow.xaml.cs
+++ HairSalon/CustomScrollBar/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,8 +21,27 @@ namespace CustomScrollBar
         {
             InitializeComponent();
         }
+
+        private void Close_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 
-
+    public class ScrollLimitConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values,Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double && values[1] is double)
+            {
+                return (double)values[0] == (double)values[1];
+            }
+            return false;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
 }
