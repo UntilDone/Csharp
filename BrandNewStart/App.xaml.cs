1de62d8b429c728cfe306d37d0c6e7af6ed99f9c
UntilDone <dbehgus0906@naver.com>
2025-05-16 22:33:37 +0900

    Brand New Starrt

---
+++ BrandNewStart/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BrandNewStart
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
