43fea9271b914f5187bd063174613835b7973be9
UntilDone <dbehgus0906@naver.com>
2025-05-21 21:30:49 +0900

    Settings

---
+++ SimpleWebTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleWebTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
